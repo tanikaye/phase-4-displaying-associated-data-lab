2381098b144ebaf5b50b2e9cf2b9c5906268ebed
Tani Kaye <1tanikaye@gmail.com>
2021-12-15 16:25:22 -0500

    commit tree 280e61265a17fd642ff9a12639456f837de5cc4c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user, include: :items
+  end
+
+  # def user_params
+  #   params.permit(:username, :city, :items)
+  # end
 end
